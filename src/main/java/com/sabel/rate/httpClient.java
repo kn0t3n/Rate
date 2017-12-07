056a491b1eefd176c9855983ee994abceb6cbd03
knoten85 <philipp.schweiger85@googlemail.com>
2017-12-07 10:40:12 +0100

    .

--- src/main/java/com/sabel/rate/httpClient.java
+++ src/main/java/com/sabel/rate/httpClient.java
@@ -1,8 +1,11 @@
 package com.sabel.rate;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.stream.JsonParser;
 import java.io.*;
 import java.net.*;
-
 
 public class httpClient {
 
@@ -23,14 +26,27 @@ public class httpClient {
         json = splits[1].substring(4, 11);
         System.out.println(json);
         br.close();
+        InputStream is = null;
+        URL urlnew = new URL("https://bitaps.com/api/ticker/average");
+        Proxy proxynew = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.100.1", 8080));
+        URLConnection urlConnectionnew = url.openConnection(proxynew);
 
-        
+        try {
+            is = urlConnectionnew.getInputStream();
+            JsonReader jsonReader = Json.createReader(is);
+            JsonObject jsonObject = jsonReader.readObject();
+            System.out.println(jsonObject.toString());
+            JsonObject fx_rates = jsonObject.getJsonObject("fx_rates");
+            System.out.println(fx_rates.getString("eur"));
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+          is.close();
+        }
+
+        JsonParser jsonParser = Json.createParser(new StringReader(json));
+
 
     }
-
-
-
-
-
 
 }
